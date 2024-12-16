1b480c34ed6ecdd78bee7c0ca6c607b043e36427
cyboran <cyboranf@gmail.com>
2024-12-16 21:58:21 +0100

    Implement User and Role entities with Liquibase changesets for database schema

---
+++ src/main/java/com/component/checkout/infrastructure/model/Role.java
@@ -0,0 +1,15 @@
+package com.component.checkout.infrastructure.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
