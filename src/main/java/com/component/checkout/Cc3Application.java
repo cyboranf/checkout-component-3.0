71d9d5263cebcb5ed998ee8e348cc3c2ab5c0c8c
cyboran <cyboranf@gmail.com>
2024-12-16 21:10:45 +0100

    init

---
+++ src/main/java/com/component/checkout/Cc3Application.java
@@ -0,0 +1,13 @@
+package com.component.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/component/checkout/Cc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.component.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
